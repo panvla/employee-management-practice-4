87e6653de0cf9e81b2ee609167310eabe46123f6
panvla <pandvlado@gmail.com>
2022-12-08 12:51:35 +0100

    added initial project

---
+++ src/main/java/com/vladimirpandurov/employee_management_app_4/EmployeeManagementApp4Application.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.employee_management_app_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementApp4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementApp4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vladimirpandurov/employee_management_app_4/EmployeeManagementApp4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.employee_management_app_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApp4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
